8283c267c5a2e4045b6a82e4bc78667ddad1af59
John Stachurski <john.stachurski@gmail.com>
2015-12-26 12:55:09 -0500

    worked on optgrowth_2 code

---
+++ optgrowth_2/growth_models.py
@@ -0,0 +1,70 @@
+"""
+Filename: growth_models.py
+Authors: John Stachurski, Thomas Sargent
+
+Some growth models, wrapped as classes.  For use with the optgrowth.py module.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from optgrowth import bellman_operator, compute_opt_growth_vf
+
+
+class LogLinearGrowthModel:
+    """
+    Stores parameters and computes solutions for the basic log utility / Cobb
+    Douglas production growth model.  Shocks are lognormal.
+    """
+    def __init__(self, 
+            alpha=0.65,         # Productivity parameter
+            beta=0.95,          # Discount factor
+            mu=1,               # First parameter in lognorm(mu, sigma)
+            sigma=0.2,          # Second parameter in lognorm(mu, sigma)
+            grid_max=3, 
+            grid_size=150):
+
+        self.alpha, self.beta = alpha, beta
+        self.grid = np.linspace(1e-6, grid_max, grid_size)
+        self.shocks = np.exp(np.random.randn(250))
+
+    def compute_value_function(self, show_plot=False):
+        v_star = compute_opt_growth_vf(self.grid, 
+                                       self.beta,
+                                       np.log,
+                                       lambda k: k**self.alpha,
+                                       self.shocks)
+
+        if show_plot:
+            fig, ax = plt.subplots()
+            ax.plot(self.grid, v_star, lw=2, alpha=0.6, label='value function')
+            ax.legend(loc='lower right')
+            plt.show()
+
+        return v_star
+
+    def compute_greedy(self, w=None, show_plot=False):
+        """
+        Compute the w-greedy policy on the grid points given w
+        (the value of the input function on grid points).  If w is not
+        supplied, use the approximate optimal value function.
+        """
+        if w is None:
+            w = self.compute_value_function()
+        Tw, sigma = bellman_operator(w, 
+                                    self.grid,  
+                                    self.beta,  
+                                    np.log,
+                                    lambda k: k**self.alpha,
+                                    self.shocks,
+                                    compute_policy=True)
+
+        if show_plot:
+            fig, ax = plt.subplots()
+            ax.plot(self.grid, sigma, lw=2, alpha=0.6, label='approximate policy function')
+            cstar = (1 - self.alpha * self.beta) * self.grid
+            ax.plot(self.grid, cstar, lw=2, alpha=0.6, label='true policy function')
+            ax.legend(loc='lower right')
+            plt.show()
+
+        return sigma
+
--- optgrowth_2/optgrowth.py
+++ optgrowth_2/optgrowth.py
@@ -2,7 +2,10 @@
 Filename: optgrowth.py
 Authors: John Stachurski, Thomas Sargent
 
-Solving the optimal growth problem via value function iteration.
+Solving the optimal growth problem via value function iteration.  The model is
+described in 
+
+    http://quant-econ.net/py/optgrowth_2.html
 """
 
 import numpy as np
@@ -10,12 +13,13 @@ from scipy.optimize import fminbound
 from scipy import interp
 from joblib import Memory
 from quantecon import compute_fixed_point
-import matplotlib.pyplot as plt
 
-# For persistent caching 
+# For persistent caching.  Here we're writing to /tmp, which only 
+# persists to the next reboot.  Change to the local dir to get a more
+# permanent cache.
 memory = Memory(cachedir='/tmp/joblib_cache')
 
-def bellman_operator(w, grid, alpha, beta, delta, Tw=None, compute_policy=0):
+def bellman_operator(w, grid, beta, u, f, shocks, Tw=None, compute_policy=0):
     """
     The approximate Bellman operator, which computes and returns the
     updated value function Tw on the grid points.  An array to store
@@ -29,12 +33,15 @@ def bellman_operator(w, grid, alpha, beta, delta, Tw=None, compute_policy=0):
         The value of the input function on different grid points
     grid : array_like(float, ndim=1)
         The set of grid points
-    alpha : scalar
-        The productivity parameter
+    u : function
+        The utility function
+    f : function
+        The production function
+    shocks : numpy array
+        An array of draws from the shock, for Monte Carlo integration (to
+        compute expectations).
     beta : scalar
         The discount factor
-    delta : scalar
-        The depreciation parameter
     Tw : array_like(float, ndim=1) optional (default=None)
         Array to write output values to
     compute_policy : Boolean, optional (default=False)
@@ -51,11 +58,10 @@ def bellman_operator(w, grid, alpha, beta, delta, Tw=None, compute_policy=0):
     if compute_policy:
         sigma = np.empty(len(w))
 
-    # == set Tw[i] = max_c { u(c) + beta w(f(k_i) + (1 - delta)k - c)} == #
-    for i, k in enumerate(grid):
-        y = k**alpha + (1 - delta) * k  # Available capital
+    # == set Tw[i] = max_c { u(c) + beta E w(f(y  - c))} == #
+    for i, y in enumerate(grid):
         def objective(c):
-            return - np.log(c) - beta * w_func(y - c)
+            return - u(c) - beta * np.mean(w_func(f(y - c) * shocks))
         c_star = fminbound(objective, 1e-10, y)
         if compute_policy:
             sigma[i] = c_star
@@ -68,7 +74,7 @@ def bellman_operator(w, grid, alpha, beta, delta, Tw=None, compute_policy=0):
 
 
 @memory.cache
-def compute_opt_growth_vf(alpha, beta, delta, grid):
+def compute_opt_growth_vf(grid, beta, u, f, shocks):
     """
     Compute the value function by iterating on the Bellman operator.
     The hard work is done by QuantEcon's compute_fixed_point function.
@@ -83,59 +89,14 @@ def compute_opt_growth_vf(alpha, beta, delta, grid):
             True, # verbose
             5,    # print_skip
             grid,
-            alpha,
             beta,
-            delta,
+            u,
+            f,
+            shocks,
             Tw=Tw,
             compute_policy=False)
-
     return v_star
 
 
-
-
-class GrowthModel:
-    """
-    A simple class to store parameters and compute solutions.
-    """
-    def __init__(self, alpha=0.65, beta=0.95, delta=1, grid_max=2, grid_size=150):
-
-        self.alpha, self.beta, self.delta = alpha, beta, delta
-        self.grid = np.linspace(1e-6, grid_max, grid_size)
-
-    def compute_value_function(self, show_plot=False):
-        v_star = compute_opt_growth_vf(self.alpha, 
-                                    self.beta, self.delta, self.grid)
-
-        if show_plot:
-            fig, ax = plt.subplots()
-            ax.plot(self.grid, v_star, lw=2, alpha=0.6, label='value function')
-            ax.legend(loc='lower right')
-            plt.show()
-
-        return v_star
-
-    def compute_greedy(self, w=None, show_plot=False):
-        """
-        Compute the w-greedy policy on the grid points given w
-        (the value of the input function on grid points).  If w is not
-        supplied, use the approximate optimal value function.
-        """
-        if w is None:
-            w = self.compute_value_function()
-        Tw, sigma = bellman_operator(w, 
-                self.grid,  
-                self.alpha,  
-                self.beta,  
-                self.delta,  
-                compute_policy=True)
-
-        if show_plot:
-            fig, ax = plt.subplots()
-            ax.plot(self.grid, sigma, lw=2, alpha=0.6, label='policy function')
-            ax.legend(loc='lower right')
-            plt.show()
-
-        return sigma
 
 
