8936b33d6cc60b7c86a9976411228635b450e4bb
John Stachurski <john.stachurski@gmail.com>
2016-07-09 08:12:56 -0400

    tidied up syntax

--- jv/jv_test.jl
+++ jv/jv_test.jl
@@ -12,7 +12,31 @@ V = compute_fixed_point(f, v_init, max_iter=200)
 s_policy, phi_policy = bellman_operator(wp, V, ret_policies=true)
 
 # === plot solution === #
-tr_phi = plot(scatter(; x = wp.x_grid, y = phi_policy, name = "ϕ policy"), Layout(xaxis_range = [0.0, maximum(wp.x_grid)], yaxis_range = [-0.1, 1.1], xaxis_title = "x"))
-tr_s = plot(scatter(; x = wp.x_grid, y = s_policy, name = "s policy"), Layout(xaxis_range = [0.0, maximum(wp.x_grid)], yaxis_range = [-0.1, 1.1], xaxis_title = "x"))
-tr_V = plot(scatter(; x = wp.x_grid, y = V, name = "Value Fn."), Layout(xaxis_range = [0.0, maximum(wp.x_grid)], xaxis_title = "x"))
+tr_phi = plot(scatter(; 
+    x = wp.x_grid, 
+    y = phi_policy, name = "ϕ policy"), 
+    Layout(xaxis_range = [0.0, 
+                    maximum(wp.x_grid)], 
+                    yaxis_range = [-0.1, 1.1], 
+                    xaxis_title = "x")
+    )
+
+tr_s = plot(scatter(; 
+    x = wp.x_grid, 
+    y = s_policy, 
+    name = "s policy"), 
+    Layout(xaxis_range = [0.0, 
+            maximum(wp.x_grid)], 
+            yaxis_range = [-0.1, 1.1], 
+            xaxis_title = "x"))
+
+tr_V = plot(scatter(; 
+    x = wp.x_grid, 
+    y = V, 
+    name = "Value Fn."), 
+    Layout(xaxis_range = [0.0, 
+                maximum(wp.x_grid)], 
+                xaxis_title = "x")
+    )
+
 display([tr_s; tr_phi; tr_V])
