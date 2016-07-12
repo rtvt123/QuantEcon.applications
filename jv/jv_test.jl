9405a50f58e894bd922d455dbb116ee9e4241f05
albep <alberto.polo@nyu.edu>
2016-07-12 14:56:10 -0400

    Updated plot programs to Plots.jl (#49)

--- jv/jv_test.jl
+++ jv/jv_test.jl
@@ -1,5 +1,6 @@
 using QuantEcon
-using PlotlyJS
+using Plots
+plotlyjs(size = (900, 900), legend = false)
 
 include("jv.jl")
 
@@ -12,31 +13,10 @@ V = compute_fixed_point(f, v_init, max_iter=200)
 s_policy, phi_policy = bellman_operator(wp, V, ret_policies=true)
 
 # === plot solution === #
-tr_phi = plot(scatter(; 
-    x = wp.x_grid, 
-    y = phi_policy, name = "phi policy"), 
-    Layout(xaxis_range = [0.0, 
-                    maximum(wp.x_grid)], 
-                    yaxis_range = [-0.1, 1.1], 
-                    xaxis_title = "x")
-    )
 
-tr_s = plot(scatter(; 
-    x = wp.x_grid, 
-    y = s_policy, 
-    name = "s policy"), 
-    Layout(xaxis_range = [0.0, 
-            maximum(wp.x_grid)], 
-            yaxis_range = [-0.1, 1.1], 
-            xaxis_title = "x"))
-
-tr_V = plot(scatter(; 
-    x = wp.x_grid, 
-    y = V, 
-    name = "Value Fn."), 
-    Layout(xaxis_range = [0.0, 
-                maximum(wp.x_grid)], 
-                xaxis_title = "x")
-    )
-
-display([tr_s; tr_phi; tr_V])
+plot(wp.x_grid, [phi_policy s_policy V],
+  title = ["phi policy" "s policy" "value function"],
+  color = [:orange :blue :green], width = 3,
+  xaxis = ("x", (0.0, maximum(wp.x_grid))),
+  yaxis = ((-0.1, 1.1)),
+  layout = (3,1), bottom_margin = 20mm, show = true)
